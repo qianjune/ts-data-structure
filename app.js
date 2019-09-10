c948911a4cdc188a858f816a74f326f529701fdc
qianjun <qjhj8ftn@gmail.com>
2019-09-10 17:57:00 +0800

    add apollo demo

--- app.js
+++ app.js
@@ -6,11 +6,26 @@ import cors from 'koa2-cors'
 import serve from 'koa-static'
 import path from 'path'
 import { ApolloServer, gql } from 'apollo-server-koa'
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = new Koa()
 app.use(cors())
 app.use(serve(path.join(__dirname, 'public/')))
 app.use(catchError)
 app.use(parser())
 InitManager.initCore(app)
+server.applyMiddleware({app})
 app.listen(3111)
 console.log('server is running on port 3111')
