3a99a72e00496336596b003e7644948f0b5a1af8
qianjun <qjhj8ftn@gmail.com>
2021-04-19 16:23:51 +0800

    refactor: common db refacotr to decorator

--- micro-services/common-service/src/db/comment.ts
+++ micro-services/common-service/src/db/comment.ts
@@ -1,55 +1,57 @@
-import { Model } from "sequelize";
+// import { Model } from "sequelize";
 import sequelize from "@root/core/db";
 import { TYPES } from "@src/db/types";
+import { Column, Table, Model, init } from "@src/lib/sequelize-ts";
+
 export enum CommentedItemType {
   PRODUCT = "product",
   NOTE = "note",
 }
+@Table({
+  sequelize,
+  tableName: "comment",
+})
 class CommentModel extends Model {
-  // custom property here
+  @Column({
+    type: TYPES.INTEGER,
+    comment: "评论id",
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+  @Column({
+    type: TYPES.STRING,
+    comment: "评论的事物的类型",
+    allowNull: false,
+  })
+  type: string;
+  @Column({
+    type: TYPES.INTEGER,
+    comment: "挂在某个商品下（或其他）",
+    allowNull: false,
+  })
+  underWhich: number;
+  @Column({
+    type: TYPES.INTEGER,
+    comment: "评论的用户id",
+    allowNull: false,
+  })
+  userId: number;
+  @Column({
+    type: TYPES.INTEGER,
+    comment: "回复父评论的id",
+    allowNull: false,
+    defaultValue: 0,
+  })
+  parentId: number;
+  @Column({
+    type: TYPES.TEXT,
+    comment: "评论的内容",
+    allowNull: false,
+  })
+  content: string;
 }
-
-CommentModel.init(
-  {
-    id: {
-      type: TYPES.INTEGER,
-      comment: "评论id",
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    type: {
-      type: TYPES.STRING,
-      comment: "评论的事物的类型",
-      allowNull: false,
-    },
-    underWhich: {
-      type: TYPES.INTEGER,
-      comment: "挂在某个商品下（或其他）",
-      allowNull: false,
-    },
-    userId: {
-      type: TYPES.INTEGER,
-      comment: "评论的用户id",
-      allowNull: false,
-    },
-    parentId: {
-      type: TYPES.INTEGER,
-      comment: "回复父评论的id",
-      allowNull: false,
-      defaultValue: 0,
-    },
-    content: {
-      type: TYPES.TEXT,
-      comment: "评论的内容",
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    tableName: "comment",
-  }
-);
-
+init(CommentModel);
 CommentModel.sync({ alter: true });
 
 export default CommentModel;
--- micro-services/common-service/src/db/safeList.ts
+++ micro-services/common-service/src/db/safeList.ts
@@ -1,52 +1,53 @@
 /**
  * @description 安全名单 数据库模型
  */
-import { Model } from "sequelize";
+// import { Model } from "sequelize";
 import sequelize from "@root/core/db";
 import { TYPES } from "@src/db/types";
 import { sequelizeErrHandler } from "@src/utils/error_handler";
 import { mysqlJsonHandler } from "@src/lib/common";
+import { Column, Table, Model, init } from "@src/lib/sequelize-ts";
 
 export enum SAFE_TYPE {
   WHITE = "white",
   BLACK = "black",
 }
-
+@Table({
+  sequelize,
+  tableName: "safeList",
+})
 class SafeList extends Model {
   // custom property here
   dataParseAttribute = ["keywordsList"];
+  @Column({
+    type: TYPES.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    comment: "主键id",
+  })
+  id: string;
+  @Column({
+    type: TYPES.STRING,
+    allowNull: false,
+    comment: "黑名单还是白名单",
+  })
+  type: string;
+  @Column({
+    type: TYPES.TEXT,
+    allowNull: false,
+    comment: "名单内容",
+    ...mysqlJsonHandler("keywordsList"),
+  })
+  keywordsList: string;
+  @Column({
+    type: TYPES.STRING,
+    allowNull: false,
+    comment: "名单名称",
+  })
+  name: string;
 }
 
-SafeList.init(
-  {
-    id: {
-      type: TYPES.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      comment: "主键id",
-    },
-    type: {
-      type: TYPES.STRING,
-      allowNull: false,
-      comment: "黑名单还是白名单",
-    },
-    keywordsList: {
-      type: TYPES.TEXT,
-      allowNull: false,
-      comment: "名单内容",
-      ...mysqlJsonHandler("keywordsList"),
-    },
-    name: {
-      type: TYPES.STRING,
-      allowNull: false,
-      comment: "名单名称",
-    },
-  },
-  {
-    sequelize,
-    tableName: "safeList",
-  }
-);
+init(SafeList);
 
 SafeList.sync({
   alter: true,
