041453d768e994378a5f9266f091568a6a04aa1a
qianjun <qjhj8ftn@gmail.com>
2021-06-11 19:21:37 +0800

    feat: add search modern

--- start.ts
+++ start.ts
@@ -1,8 +1,8 @@
 import "module-alias/register";
 import "reflect-metadata";
-// import { quickSort, QuickSort } from "@src/algorithm/quick-sort";
-// QuickSort.sort<number>([4, 6, 5, 2, 3, 8, 7, 1]);
-import "@src/algorithm/insert-sort";
+import { quickSort, QuickSort } from "@src/algorithm/quick-sort";
+QuickSort.sort<number>([4, 6, 5, 2, 3, 8, 7, 1]);
+// import "@src/algorithm/insert-sort";
 // rx 学习
 // import './experiment/rx-dev/index'
 // 算法
