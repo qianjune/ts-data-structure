b5f6880768cd323a7f6960b29c96266f67795ebf
qianjun <qjhj8ftn@gmail.com>
2021-06-18 16:17:52 +0800

    feat: Set add base

--- src/algorithm/binary-search-tree/index2.ts
+++ src/algorithm/binary-search-tree/index2.ts
@@ -4,7 +4,7 @@
  * 每个节点都大于它左边的左右的子节点，小于右边的子节点
  */
 
-class BTS2<E>{
+class BTS2<E> {
   private root: Node<E>;
   private size: number;
   constructor() {
@@ -13,26 +13,26 @@ class BTS2<E>{
   }
 
   public getSize(): number {
-    return this.size
+    return this.size;
   }
 
   isEmpty(): boolean {
     return this.size === 0;
   }
   public add(e: E) {
-    this.root = this._add(this.root, e)
+    this.root = this._add(this.root, e);
   }
   private _add(node: Node<E>, e: E): Node<E> {
     if (node === null) {
-      this.size++
-      return new Node(e)
+      this.size++;
+      return new Node(e);
     }
     if (e > node.e) {
-      node.right = this._add(node.left, e)
+      node.right = this._add(node.left, e);
     } else if (e < node.e) {
-      node.left = this._add(node.right, e)
+      node.left = this._add(node.right, e);
     }
-    return node
+    return node;
   }
   contain() { }
   search() { }
---
+++ src/data-structure/set/index.ts
@@ -0,0 +1,38 @@
+/**
+ * @description Set
+ * 不能添加重复元素
+ * 适合用于统计
+ */
+
+export interface Set<E> {
+  add(e: E): void;
+  remove(e: E): void;
+  contains(e: E): boolean;
+  getSize(): number;
+  isEmpty(): boolean;
+}
+
+class BSTSet<E> implements Set<E> {
+  private bst: BTS2<E>;
+  constructor() {
+    this.bst = new BTS2<E>();
+  }
+  add(e: E): void {
+    this.bst.add(e);
+  }
+  remove(e: E): void {
+    throw new Error("Method not implemented.");
+  }
+  contains(e: E): boolean {
+    throw new Error("Method not implemented.");
+  }
+  isEmpty(): boolean {
+    return this.bst.getSize() === 0;
+  }
+
+  getSize(): number {
+    return this.bst.getSize();
+  }
+}
+
+export { BSTSet };
