67725d6857b682f6e5d9b1654121f66ca30793c2
qianjun <qjhj8ftn@gmail.com>
2021-07-22 18:21:48 +0800

    feat: 高频问题-反转烧饼

---
+++ src/labuladong-study/高频题/pancakeSort.ts
@@ -0,0 +1,13 @@
+/**
+ * @description 反转烧饼
+ * 解题方式：递归
+ */
+const reverse = () => { };
+const sort = (cakes: number[], n: number) => {
+  // 找到当前最大的一个烧饼
+  // 从那个位置反转一次，把最大的放到最上面
+  // 然后在从n-1反转一次，把最大的反转到当前的最下面
+};
+const pancakeSort = (cakes: number[]): number[] => {
+  const res = []; // 存入所有的反转过程
+};
