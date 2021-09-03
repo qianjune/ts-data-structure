bc771a76b6c18c510f32b91c58bfa2dc1456d53e
qianjun <qjhj8ftn@gmail.com>
2021-09-03 17:33:37 +0800

    feat: 牛客 - NC2 重排链表

--- start.ts
+++ start.ts
@@ -1,5 +1,7 @@
 import "module-alias/register";
 import "reflect-metadata";
+import { LinkedListUtil } from "@src/labuladong-study/linkedList/util";
+import { ListNode } from "@src/labuladong-study/public/linkedList";
 // import { BiscuitsSort } from "@src/question/biscuits-sort";
 // import "@src/data-structure/linkedList";
 // import "@src/algorithm/binary-search-tree/index2";
@@ -23,4 +25,47 @@ import "reflect-metadata";
 // import "@src/data-structure/tree/red-black-tree/index";
 // import "@src/algorithm/sort/base-sort/msd";
 // import "@src/question/string-match/index";
-import "./app";
+function reorderList(head: ListNode) {
+  if (!head || !head.next || !head.next.next) return head;
+  function reserve(head: ListNode) {
+    let cur = head;
+    let pre = null;
+    while (cur) {
+      const next = cur.next;
+      cur.next = pre;
+      pre = cur;
+      cur = next;
+    }
+    return pre;
+  }
+  function merge(head: ListNode, newHead: ListNode) {
+    while (newHead) {
+      const rest = newHead.next;
+      newHead.next = head.next;
+      head.next = newHead;
+      head = newHead.next;
+      newHead = rest;
+    }
+  }
+  // write code here
+  // 寻找链表重点
+  let left = head;
+  let right = head.next;
+  while (right && right.next) {
+    left = left.next;
+    right = right.next.next;
+  }
+  // 逆序后半部分链表
+  const newHead = reserve(left.next);
+  left.next = null;
+
+  LinkedListUtil.loopList(head);
+  LinkedListUtil.loopList(newHead);
+  // 循环合并前后链表
+  merge(head, newHead);
+  console.log(head.val);
+}
+const head = LinkedListUtil.build(3);
+reorderList(head);
+LinkedListUtil.loopList(head);
+// import "./app";
